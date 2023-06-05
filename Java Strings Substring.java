ba66c81ce876586081a4ad7c06077409ac5120fc
Aurelie1234 <115810657+Aurelie1234@users.noreply.github.com>
2023-06-05 16:29:35 +0000

    Create Java Strings Substring.java

---
+++ Java Strings Substring.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+String S = in.next();
+int start = in.nextInt();
+int end = in.nextInt();
+String s2 = S.substring(start, end);
+System.out.println(s2);
+}
+}
